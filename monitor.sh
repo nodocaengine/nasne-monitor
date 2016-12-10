6efa70cf66b323b54b66c343177e5a97332c4ebf
nodoca <nodocaengine@gmail.com>
2016-12-10 12:24:17 +0900

    🎉

---
+++ monitor.sh
@@ -0,0 +1,19 @@
+{
+  "mappings": {
+    "usage": {
+      "properties": {
+        "titles": {
+          "type": "integer"
+        },
+        "usage": {
+          "type": "float"
+        },
+        "timestamp": {
+          "type": "date",
+          "format": "date_time_no_millis"
+        }
+      }
+    }
+  }
+}
+
